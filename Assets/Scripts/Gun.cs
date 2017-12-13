d479b259faa558ce6ec89b83a8e612b8b0fcee5b
Antonio <antonio.ruggiero93@hotmail.it>
2017-12-13 23:53:55 +0100

    Projectile prefab, muzzle particles, some objects in village are interactable

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,37 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     private Transform _emissionTransform;
+    public GameObject ProjectilePrefab;
+
+    private SteamVR_TrackedController _controller;
 
     private void Start()
     {
         _emissionTransform = transform.Find("Emission");
-        GetComponentInParent<SteamVR_TrackedController>().TriggerClicked += (sender, args) =>
-        {
-            Shot();
-        };
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-          Shot();
+            Shot();
         }
     }
 
+    public void Equip()
+    {
+        _controller = GetComponentInParent<SteamVR_TrackedController>();
+        if (_controller)
+        {
+            _controller.TriggerClicked += ControllerOnTriggerClicked;
+        }
+    }
+
+    public void Release()
+    {
+        _controller.TriggerClicked -= ControllerOnTriggerClicked;
+    }
+
+    private void ControllerOnTriggerClicked(object o, ClickedEventArgs clickedEventArgs)
+    {
+        Shot();
+    }
 
     void Shot()
     {
         RaycastHit hit;
-        var ray=new Ray(_emissionTransform.position,_emissionTransform.forward);
-        Debug.DrawRay(ray.origin,ray.GetPoint(10),Color.green,0.3f);
+        var ray = new Ray(_emissionTransform.position, _emissionTransform.forward);
+        Debug.DrawRay(ray.origin, ray.GetPoint(10), Color.green, 0.3f);
         if (Physics.Raycast(ray, out hit))
         {
             hit.collider.GetComponent<HitZone>()?.Hit(gameObject, hit.point);
         }
+        _emissionTransform.GetComponentInChildren<ParticleSystem>().Play();
+        Instantiate(ProjectilePrefab, _emissionTransform.position, _emissionTransform.rotation);
     }
 }
